5ba5173c20553287c1d0a92fcf4308659bf07fc0
Paulo Correa <paulo.correa@indt.org.br>
2019-08-23 17:31:34 -0400

    add TaskServiceImp and TaskController

--- src/main/java/com/pcorrea/tasks/controller/TaskController.java
+++ src/main/java/com/pcorrea/tasks/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.pcorrea.tasks.controller;
 
 import com.pcorrea.tasks.domain.Task;
+import com.pcorrea.tasks.service.TaskService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/tasks")
 public class TaskController {
 
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping( value = {"","/"})
-    public Iterable<Task> list(){
-        return null;
+    public Iterable<Task> list() {
+        return this.taskService.list();
     }
 }
--- src/main/java/com/pcorrea/tasks/service/TaskServiceImp.java
+++ src/main/java/com/pcorrea/tasks/service/TaskServiceImp.java
@@ -1,13 +1,18 @@
 package com.pcorrea.tasks.service;
 
 import com.pcorrea.tasks.domain.Task;
-
+import com.pcorrea.tasks.repository.TaskRepository;
 
 public class TaskServiceImp implements TaskService {
 
+    private TaskRepository taskRepository;
+
+    public TaskServiceImp(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     @Override
     public Iterable<Task> list() {
-        return null;
+        return this.taskRepository.findAll();
     }
 }
